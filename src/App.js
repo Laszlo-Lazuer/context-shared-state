0ef7c3974a94ffe0942d2db5e92ae7f8efeb1d2d
Laszlo Lazuer <visualjazz@gmail.com>
2018-11-13 14:59:32 -0600

    functional.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ButtonA from './ButtonA.component';
+import ButtonB from './ButtonB.component';
+import { SharedSnackbarProvider } from './SharedSnackbar.context';
+
+const styles = {
+  app: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+  },
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={styles.app}>
+        <SharedSnackbarProvider>
+          <ButtonA />
+          <ButtonB />
+        </SharedSnackbarProvider>
       </div>
     );
   }
